94b849c9e34908daf966f3257e468207e4ff6765
RachitSharma2001 <rachitsharma613@gmail.com>
2021-08-22 17:42:27 -0700

    Sign up hashes password, login works, logout

--- app/models.py
+++ app/models.py
@@ -1,11 +1,23 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     major = db.Column(db.String(100))
     hall = db.Column(db.String(100))
     password = db.Column(db.String(100))
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, given_password):
+        return check_password_hash(self.password, given_password)
+
     def __repr__(self):
         return 'User {}'.format(self.name)
+    
+@login.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from flask_login import current_user, login_user, logout_user
 from app import app, db
 from app.forms import LoginForm, SignupForm
 from app.models import Users
@@ -7,7 +8,28 @@ from app.models import Users
 @app.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        # if the user is already logged in, redirect
+        if current_user.is_authenticated:
+            return redirect(url_for("message"))
+        
+        user = Users.query.filter_by(name=login_form.data['name']).first()
+        if user is None or not user.check_password(login_form.data['password']):
+            print("Incorrect password: ", login_form.data['password'])
+            print("User: ", user)
+            print("Check password: ", user.check_password(login_form.data['password']))
+            print("Check password hi: ", user.check_password("hi"))
+            return redirect(url_for("login"))
+        
+        login_user(user)
+        return redirect(url_for("message"))
+
     return render_template("login.html", form=login_form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
@@ -18,12 +40,14 @@ def signup():
         major = signup.data['major']
         hall = signup.data['hall']
         password = signup.data['password']
+        
+        user = Users(name=name, major=major, hall=hall)
+        user.set_password(password)
 
-        user = Users(name=name, major=major, hall=hall, password=password)
         db.session.add(user)
         db.session.commit()
         
-        return login()
+        return redirect(url_for("login"))
 
     return render_template("signup.html", form=signup)
 
